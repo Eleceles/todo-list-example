60a45b0b47ad54efc18fbf52db4e608eb5b3d358
Eleceles <91483876+Eleceles@users.noreply.github.com>
2021-11-22 23:37:57 +0000

    Dev (#20)
    * added read tasks funtionality (#6) (#7)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * ordered tasks by complete/incpmplete (#8)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * Added create task form (#9)
    
    * added new task form
    
    * added a navbar
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * added update form for tasks (#10)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * added delete button and changed 'update' button to say 'edit' (#11)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * added complete/incomplete buttons (#14)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * Update app.py
    
    * adding unit test (#15)
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * Feature/13 adding unit tests (#16)
    
    * adding unit test
    
    * adding jenkinsfile
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * Feature/13 adding unit tests (#18)
    
    * adding unit test
    
    * adding jenkinsfile
    
    * adding jenkinsfile
    
    * update jenkinsfile
    
    * update jenkinsfile
    
    * update jenkinsfile
    
    * add post build actions to jenkinsfile
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update requirements.txt
    
    * Update app.py
    
    * Feature/Jenkins docker (#19)
    
    * adding unit test
    
    * adding jenkinsfile
    
    * adding jenkinsfile
    
    * update jenkinsfile
    
    * update jenkinsfile
    
    * update jenkinsfile
    
    * add post build actions to jenkinsfile
    
    * added jenkins and docker files
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>
    
    Co-authored-by: Ubuntu <eleana@development-server.31wiqa5zmbdu1pb4lahxfyfnbg.zx.internal.cloudapp.net>

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db
 from application.models import Tasks
-from flask import render_template
+
 
 @app.route('/')
 @app.route('/home')
@@ -8,13 +8,17 @@ def home():
     all_tasks = Tasks.query.all()
     return render_template('index.html', title="Home", all_tasks=all_tasks)
     
+@app.route('/create/task', methods=['GET', 'POST'])
+def create_task():
+    form = TaskForm()
 
-@app.route('/create/task')
-def add():
-    new_task = Tasks(description="New Task")
-    db.session.add(new_task)
-    db.session.commit()
-    return f"Task with id {new_task.id} added to database"
+    if request.method == "POST": 
+       new_task = Tasks(description=form.description.data)
+       db.session.add(new_task)
+       db.session.commit()
+       return redirect(url_for('home'))
+
+    return render_template("create_task.html", title="Add a new Task", form=form)
 
 @app.route('/read/allTasks')
 def read_tasks():
@@ -30,30 +34,21 @@ def read_tasks():
 
     return tasks_dict
 
-@app.route('/update/task/<int:id>/<new_description>')
-def update_task(id, new_description):
+@app.route('/update/task/<int:id>',  methods=['GET','POST'])
+def update_task(id):
+    form = TaskForm()
     task = Tasks.query.get(id)
-    task.description = new_description
-    db.session.commit()
-    return f"Task {id} update to {new_description}"
+
+    if request.method == "POST": 
+       task.description = form.description.data
+       db.session.commit()
+       return redirect(url_for('home'))
+        
+    return render_template('update_task.html', task=task, form=form)
+
 
 @app.route('/delete/task/<int:id>')
 def delete_task(id):
     task = Tasks.query.get(id)
     db.session.delete(task)
     db.session.commit()
-    return f"Task {id} deleted"
-
-@app.route('/complete/task/<int:id>')
-def complete_task(id, new_description):
-    task = Tasks.query.get(id)
-    task.completed = True
-    db.session.commit()
-    return f"Task {id} has been completed"
-
-@app.route('/incomplete/task/<int:id>')
-def incomplete_task(id, new_description):
-    task = Tasks.query.get(id)
-    task.completed = False 
-    db.session.commit()
-    return f"Task {id} has been set to incompleted"
