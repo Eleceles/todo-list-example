9ae51487a001798bf0a6c56ab0cee8859db3939f
Ubuntu <eleana@dev-jenkins.mtxh4jvb1zqe1jaqpaeax4mkmg.cwx.internal.cloudapp.net>
2021-11-26 10:55:51 +0000

    fixing backend indentation app.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,6 @@ from os import getenv
 if __name__ == "__main__":
     if getenv("CREATE_SCHEMA") is not None:
         if getenv("CREATE_SCHEMA").lower() == "true":
-        db.drop_all()
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0') 
+            db.drop_all()
+            db.create_all()
+    app.run(debug=True, host='0.0.0.0')
