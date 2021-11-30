360f44020c1e12c5a3c81ef3d02c06ef2bf50c27
Ubuntu <eleana@dev-jenkins.mtxh4jvb1zqe1jaqpaeax4mkmg.cwx.internal.cloudapp.net>
2021-11-30 08:21:31 +0000

    addind test script

--- frontend/tests/test_unit.py
+++ frontend/tests/test_unit.py
@@ -1,28 +1,17 @@
 from flask import url_for
 from flask_testing import TestCase
-from application import app, db
-from application.models import Tasks
+from application import app
+
 
 class TestBase(TestCase):
 
     def create_app(self):
         # Defines the flask object's configuration for the unit tests
         app.config.update(
-            SQLALCHEMY_DATABASE_URI='sqlite:///',
             DEBUG=True,
             WTF_CSRF_ENABLED=False
         )
         return app
-    def setUp(self):
-        # Will be called before every test
-        db.create_all()
-        db.session.add(Tasks(description="Run unit tests"))
-        db.session.commit()
-
-    def tearDown(self):
-        # Will be called after every test
-        db.session.remove()
-        db.drop_all()
 
 class TestViews(TestBase):
     # Test whether we get a successful response from our routes
@@ -48,10 +37,6 @@ class TestRead(TestBase):
         response = self.client.get(url_for('home'))
         self.assertIn(b"Run unit tests", response.data)
     
-    def test_read_tasks_dictionary(self):
-        response = self.client.get(url_for('read_tasks'))
-        self.assertIn(b"Run unit tests", response.data)
-
 class TestCreate(TestBase):
     def test_create_task(self):
         response = self.client.post(url_for('create_task'), data={"description": "Testing create funtionality"}, follow_redirects=True)
@@ -62,13 +47,13 @@ class Testupdate(TestBase):
         response = self.client.post(url_for('update_task', id=1), data={"description": "Testing update funtionality"}, follow_redirects=True)
         self.assertIn(b"Testing update funtionality", response.data)
 
-    def test_complete_task(self):
-        response = self.client.get(url_for('complete_task', id=1),follow_redirects=True)
-        self.assertEqual(Tasks.query.get(1).completed, True)
+    # def test_complete_task(self):
+    #     response = self.client.get(url_for('complete_task', id=1),follow_redirects=True)
+    #     self.assertEqual(Tasks.query.get(1).completed, True)
     
-    def test_incomplete_task(self):
-        response = self.client.get(url_for('incomplete_task', id=1),follow_redirects=True)
-        self.assertEqual(Tasks.query.get(1).completed, False)
+    # def test_incomplete_task(self):
+    #     response = self.client.get(url_for('incomplete_task', id=1),follow_redirects=True)
+    #     self.assertEqual(Tasks.query.get(1).completed, False)
 
 class TestDelete(TestBase):
     def test_delete_task(self):
